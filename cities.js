fa9b3b46ea9f91a5a54775bf73b78c32e8175b72
Chris Pascual <christopher.pascual@gmail.com>
2017-04-06 13:28:48 -0400

    map

---
+++ cities.js
@@ -0,0 +1,137 @@
+var city = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "Hershey",
+        "Population": 565.006,
+        "Median": "58,166.00",
+        "Poverty": 10.5,
+        "Employment": 1.7169,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.65298461914062,
+          40.28895415740959
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "State College",
+        "Population": 160.58,
+        "Median": "56,337.00",
+        "Poverty": 16.8,
+        "Employment": 1.7169,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.86148071289062,
+          40.79301881008675
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "Williamsport",
+        "Population": 116.048,
+        "Median": "51,455.00",
+        "Poverty": 15.2,
+        "Employment": -5.0343,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.00489044189453,
+          41.24890252240322
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "Scranton",
+        "Population": 558.166,
+        "Median": "49,670.00",
+        "Poverty": 15.2,
+        "Employment": -0.7692,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.66215515136719,
+          41.408230864902784
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "Allentown",
+        "Population": 832.327,
+        "Median": "60,671.00",
+        "Poverty": 9.9,
+        "Employment": -0.1036,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.47332763671875,
+          40.602484146302075
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "class": "city",
+        "marker-size": "medium",
+        "marker-symbol": "marker",
+        "City": "Philadelphia",
+        "Population": 6069.875,
+        "Median": "51.455.00",
+        "Poverty": 13.1,
+        "Employment": 2.954,
+        "dx": "0em",
+        "dy": "1.25em"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.333251953125,
+          39.863371338285305
+        ]
+      }
+    }
+  ]
+}
---
+++ statesLabels.js
@@ -0,0 +1,53 @@
+var stateLabels = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "PENNSYLVANIA",
+                "class": "state-label",
+                "dx" : "0em",
+                "dy" : "2.5em"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -79.69482421875,
+                    41.15384235711447
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "NEW JERSEY",
+                "class": "state-label",
+                "dx" : "0.2em",
+                "dy" : "0em"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -74.55322265625,
+                    40.79717741518766
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "DELAWARE",
+                "class": "alt-state-label",
+                "dx" : "-0.5em",
+                "dy" : "0em"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -76.22314453125,
+                    39.487084981687495
+                ]
+            }
+        }
+    ]
+}
